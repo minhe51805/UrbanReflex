9125dc61915f8dc645cb84f77a93f1e2a7b12856
minhe51805 <truongminh0949@gmail.com>
2025-12-03 22:21:51 +0700

    docs: update author header in CodeExample component

--- urbanreflex-fe/components/home/CodeExample.tsx
+++ urbanreflex-fe/components/home/CodeExample.tsx
@@ -1,7 +1,7 @@
 /**
  * Author: Trương Dương Bảo Minh (minhe51805)
  * Create at: 13-11-2025
- * Update at: 15-11-2025
+ * Update at: 01-12-2025
  * Description: Code example component displaying API usage examples in a terminal-style interface
  */
 
@@ -19,7 +19,7 @@ export default function CodeExample() {
           </div>
           <span className="text-gray-400 text-sm ml-4">urbanreflex.R</span>
         </div>
-        
+
         {/* Code content */}
         <div className="p-6 font-mono text-sm">
           <div className="space-y-2">
@@ -29,9 +29,9 @@ export default function CodeExample() {
               <span className="text-[#98c379]">&quot;urbanreflex&quot;</span>
               <span className="text-white">)</span>
             </div>
-            
+
             <div className="h-4"></div>
-            
+
             <div>
               <span className="text-white">locations </span>
               <span className="text-[#56b6c2]">&lt;-</span>
@@ -40,21 +40,21 @@ export default function CodeExample() {
               <span className="text-[#61afef]">list_locations</span>
               <span className="text-white">(</span>
             </div>
-            
+
             <div className="pl-4">
               <span className="text-[#e06c75]">parameters_id</span>
               <span className="text-white"> = </span>
               <span className="text-[#d19a66]">2</span>
               <span className="text-white">,</span>
             </div>
-            
+
             <div className="pl-4">
               <span className="text-[#e06c75]">radius</span>
               <span className="text-white"> = </span>
               <span className="text-[#d19a66]">10000</span>
               <span className="text-white">,</span>
             </div>
-            
+
             <div className="pl-4">
               <span className="text-[#e06c75]">coordinates</span>
               <span className="text-white"> = </span>
@@ -69,7 +69,7 @@ export default function CodeExample() {
               <span className="text-[#56b6c2]">-36.7726</span>
               <span className="text-white">)</span>
             </div>
-            
+
             <div>
               <span className="text-white">)</span>
             </div>
