8e2b649ce822123e7af88e9bcf159b043e0a53fc
minhe51805 <truongminh0949@gmail.com>
2025-12-03 22:22:05 +0700

    docs: update author header in why open data page

--- urbanreflex-fe/app/why-open-data/page.tsx
+++ urbanreflex-fe/app/why-open-data/page.tsx
@@ -1,7 +1,7 @@
 /**
  * Author: Trương Dương Bảo Minh (minhe51805)
  * Create at: 16-11-2025
- * Update at: 16-11-2025
+ * Update at: 01-12-2025
  * Description: Why Open Data page explaining the importance of open data and its benefits for air quality monitoring
  */
 
